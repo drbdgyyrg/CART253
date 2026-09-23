d9630f0c24f450a69a9c6b3d05e21686b4988e4f
Runzhuo <drbdgyyrg@hotmail.com>
2026-09-22 23:03:34 -0400

    add details on the soccer field and added journal file

---
+++ topics/instructions-prototype-1/journal.md
@@ -0,0 +1,10 @@
+# Instruction Prototype Soccerfield
+
+Runzhuo Zhang
+
+[View this project online](URL_FOR_THE_RUNNING_PROJECT)
+
+## Reflective Journal
+
+I feel this project is hard because my p5js skill set has so many limitation. Originally, I was trying to do a **strawberry** but I could not make a shape of strawberry body with curvature. I did research online and tried **BezierVertex** but it didn't work. After that I changed my idea and did a soccer field; it is a much easier process but the difficulty is the accuracy. Therefore, I did data research of soccer field such as the size of penalty box, goal, arc on so on. And then I calculated those numbers and filled in my Javascript. I feel such an accomplishment by doing it.
+
--- topics/instructions-prototype-1/js/script.js
+++ topics/instructions-prototype-1/js/script.js
@@ -30,8 +30,32 @@ function draw() {
     strokeWeight(2);
     rect(30,30, 840, 544);
     pop();
+    drawarc();
     drawcircle();
     drawlines();
+    drawspots();
+}
+function drawspots(){
+    push();
+    fill(255);
+    noStroke();
+    circle(120, 302, 3.5);
+    circle(780, 302, 3.5);
+    circle(450, 302, 5);
+    pop();
+}
+function drawarc(){
+    push();
+    fill(150, 220, 150);
+    stroke(255);
+    strokeWeight(2);
+    arc(120, 302, 146.4, 146.4, radians(305), radians(55));
+    arc(780, 302, 146.4, 146.4, radians(125), radians(235));
+    arc(30,30, 20,20, radians(0), radians(90));
+    arc(870,30, 20,20, radians(90), radians(180));
+    arc(30,574, 20,20, radians(270), radians(360));
+    arc(870,574, 20,20, radians(180), radians(270));
+    pop();
 }
 function drawcircle(){
     push();
@@ -45,7 +69,29 @@ function drawlines(){
     push();
     stroke(255);
     strokeWeight(2);
+    //draw center line
     line(450,30, 450, 574);
+    //draw big box
+    line(30,140.72, 162, 140.72);
+    line(30,463.28, 162, 463.28);
+    line(738,140.72, 870, 140.72);
+    line(738,463.28, 870, 463.28);
+    line(162,140.72, 162, 463.28);
+    line(738,140.72, 738, 463.28);
+    //draw small box
+    line(30, 198.8, 74, 198.8);
+    line(30, 404.2, 74, 404.2);
+    line(870, 198.8, 826, 198.8);
+    line(870, 404.2, 826, 404.2);
+    line(74,198.8,74,404.2);
+    line(826,198.8,826,404.2);
+    //draw goal
+    line(15,272.72, 15,331.28);
+    line(885,272.72, 885,331.28);
+    line(15,272.72, 30,272.72);
+    line(885,272.72, 870,272.72);
+    line(15,331.28, 30,331.28);
+    line(885,331.28, 870,331.28);
     pop();
 }
 function drawStrawberryCurve(){
